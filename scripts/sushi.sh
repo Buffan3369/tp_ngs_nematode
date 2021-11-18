55295f93675499e14653c367acfb93cb4c8930e6
Lucas <lucas.buffan@ens-lyon.fr>
2021-11-18 16:29:01 +0000

    functional salmon implementation

--- scripts/sushi.sh
+++ scripts/sushi.sh
@@ -1,5 +1,34 @@
+mkdir -p /home/rstudio/mydatalocal/tp_ngs_nematode/results/alignment #directory where we'll store salmon's outputs
+
 cd /home/rstudio/mydatalocal/tp_ngs_nematode/data/reference_transcriptome
 
-/softwares/salmon-1.5.2_linux_x86_64/
+ref_transcriptome=Caenorhabditis_elegans.WBcel235.cdna.all.fa
 
-salmon index -t Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz -i ref_index
+#indexing reference transcriptome
+
+ind=/home/rstudio/mydatalocal/tp_ngs_nematode/results/alignment/ref_idx
+
+salmon index -t $ref_transcriptome \
+-i $ind
+
+#pseudo-mapping the transcripts with this ref
+
+Names="SRR5564855
+      SRR5564856
+      SRR5564857
+      SRR5564867
+      SRR5564868
+      SRR5564869"
+
+for transcript in $Names
+do
+  salmon quant \
+  --libType A \
+  --index $ind \
+  --mates1 /home/rstudio/mydatalocal/tp_ngs_nematode/results/trimming/${transcript}_forward_paired.fq.gz \
+  --mates2 /home/rstudio/mydatalocal/tp_ngs_nematode/results/trimming/${transcript}_reverse_paired.fq.gz \
+  --threads 3 \
+  --output /home/rstudio/mydatalocal/tp_ngs_nematode/results/alignment/${transcript}_quant
+done
+
+# --threads 3 va utiliser 3 coeurs => parallélisation du programme
