254e9b2197fef8513cf15749109be3464b0f11e6
Lucas <lucas.buffan@ens-lyon.fr>
2021-11-18 16:29:23 +0000

    typo

--- scripts/whole_transcriptome.sh
+++ scripts/whole_transcriptome.sh
@@ -1,8 +1,8 @@
 #COMPLETE NEMATODE GENOME
 
 cd /home/rstudio/mydatalocal/tp_ngs_nematode/data
-mkdir reference_genome
-cd reference_genome
+mkdir reference_transcritome
+cd reference_transcriptome
 
 wget http://ftp.ensembl.org/pub/release-104/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz
 
