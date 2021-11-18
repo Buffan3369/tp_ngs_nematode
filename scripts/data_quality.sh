f6aea0d0b584ab4bd6dc76eec7e2b1262df18dd5
Lucas <lucas.buffan@ens-lyon.fr>
2021-11-18 13:11:32 +0000

    modif

--- scripts/data_quality.sh
+++ scripts/data_quality.sh
@@ -2,4 +2,4 @@
 
 fastqc -o /home/rstudio/mydatalocal/tp_ngs_nematode/results/processed_data /home/rstudio/mydatalocal/tp_ngs_nematode/data/*.gz
 
-multiqc -o /home/rstudio/mydatalocal/tp_ngs_nematode/results/processed_data/multiqc processed_data
+multiqc -o /home/rstudio/mydatalocal/tp_ngs_nematode/results/processed_data/multiqc /home/rstudio/mydatalocal/tp_ngs_nematode/results/processed_data
