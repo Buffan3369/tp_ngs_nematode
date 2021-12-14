40dac213eca65778e8f3aa0e179cd9283c9296e2
Lucas <lucas.buffan@ens-lyon.fr>
2021-12-14 15:34:06 +0000

    ending modifications

--- scripts/DE_quantification.R
+++ scripts/DE_quantification.R
@@ -13,6 +13,7 @@ NAMES <- c("SRR5564855",
            "SRR5564869")
 
 
+
 #### we specify the NAMES of the final directories ####
 
 Dir_fin <- c() #list of the 6 final directories
@@ -24,13 +25,18 @@ for(name in NAMES){
 files <- file.path(dir, Dir_fin) #the NAMES of the salmon outputs
 
 
+
 #### we then run tximport ####
 
+path_ref_names <- "~/mydatalocal/tp_ngs_nematode/data/tx2gn.csv"
+tx2gn <- read.table(path_ref_names, h=T, sep=',', as.is=T) #nomenclature change (to further be able to extract names that are recognized by WormBase)
 txi_salmon <- tximport(files,
                        type = 'salmon', 
-                       txOut = TRUE           #we output the transcript-level
+                       tx2gene = tx2gn           #we output the transcript-level
                        )
 head(txi_salmon$counts)
+
+
 
 
 #### We create a DESeq object from txi_salmon ####
@@ -45,6 +51,21 @@ dds <- DESeqDataSetFromMatrix(round(txi_salmon$counts), colData = ColData , desi
 
 quantif <- DESeq(dds, test = "Wald")
 res <- results(quantif) #extracts log2(fold_change) and form DESeq output
+res
 
-plot(x = res$log2FoldChange, y = log(res$pvalue)) #raw plot
-plotMA(res)
+plot(x = res$log2FoldChange, y = -log10(res$pvalue), ylim = c(0,4)) #raw plot
+plotMA(res, ylim = c(-12,12))  #nice plot
+
+plot(res$padj)
+
+
+### Extract the output table of DESeq ###
+
+pval_threshold = 0.05
+fold_change_threshold = 0.5  #article
+
+#get the indexes of relevant sequences
+ind <- na.omit(rownames(res)[ (res$padj <= pval_threshold) & (res$log2FoldChange > fold_change_threshold) ])
+length(ind)
+cat(ind)     #we finally select 200 genes significantly differentially expressed in the mutant lines (~10% C.elegans' genome)
+
